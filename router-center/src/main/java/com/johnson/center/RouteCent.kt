d09590fb66927e7eb013a4d77d1186d405619ebe
Johnson <poiuytrngo@qq.com>
2021-07-12 20:16:48 +0800

    项目结构调整

---
+++ router-center/src/main/java/com/johnson/center/RouteCent.kt
@@ -0,0 +1,11 @@
+package com.johnson.center
+
+import android.content.Context
+
+object RouteCent {
+    private var context: Context? = null
+    fun init(context: Context) {
+        this.context = context
+
+    }
+}
